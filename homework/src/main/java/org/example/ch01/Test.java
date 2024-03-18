c9ac2412bed163a8f5bf821516c18dec86899c31
xeulbn <gsbtiger0215@gmail.com>
2024-03-18 11:15:46 +0900

    숙제 완료

--- homework/src/main/java/org/example/ch01/Test.java
+++ homework/src/main/java/org/example/ch01/Test.java
@@ -2,6 +2,8 @@ package org.example.ch01;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("숙제를 하세요^^");
+
+        System.out.println("숙제를 하였습니다.");
     }
 }
