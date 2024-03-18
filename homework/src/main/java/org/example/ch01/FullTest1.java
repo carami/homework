7fb58b0588661ce8fea1a5ad7bd0a750b0ca6d3a
flexycrow2 <hanjw1229@hanyang.ac.kr>
2024-03-18 16:46:58 +0900

    test11

--- homework/src/main/java/org/example/ch01/FullTest1.java
+++ homework/src/main/java/org/example/ch01/FullTest1.java
@@ -2,6 +2,7 @@ package org.example.ch01;
 
 public class FullTest1 {
     public static void main(String[] args) {
-        System.out.println("fulltest1");
+        System.out.println("fulltest11");
+        System.out.println("fulltest11");
     }
 }
