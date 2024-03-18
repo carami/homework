41417bd09de02aaa233c85b7c5ae5e8a7f3ba034
honeyWater <tkd2756@naver.com>
2024-03-18 11:39:31 +0900

    pull request 테스트

--- homework/src/Exam02.java
+++ homework/src/Exam02.java
@@ -2,6 +2,6 @@ package src;
 
 public class Exam02 {
     public static void main(String[] args) {
-        System.out.println("pull request test");
+        System.out.println("풀 리퀘스트 테스트");
     }
 }
