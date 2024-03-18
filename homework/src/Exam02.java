6cac0778f68ee65b229e33a818591c3db8e4828b
honeyWater <tkd2756@naver.com>
2024-03-18 12:55:32 +0900

    test pull request

--- homework/src/Exam02.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class Exam02 {
-    public static void main(String[] args) {
-        System.out.println("풀 리퀘스트 테스트");
-    }
-}
--- homework/src/main/java/org/example/ch01/Exam01.java
+++ homework/src/main/java/org/example/ch01/Exam01.java
@@ -1,7 +1,8 @@
+package org.example.ch01;
+
 public class Exam01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("pull request test");
-        System.out.println("풀리퀘스트 테스트...");
     }
 }
